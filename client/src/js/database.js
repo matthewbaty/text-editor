ba5a4971682e9cd72c3eda66434bb8d958cf40aa
Matthew Baty <mattbaty@outlook.com>
2023-06-24 11:56:47 -0400

    // TODO: Add logic to a method that accepts some content and adds it to the database. finished

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,7 +13,18 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  try {
+    const db = await openDB('jate', 1);
+    const tx = db.transaction('jate', 'readwrite');
+    const store = tx.objectStore('jate');
+    await store.add({ content: content });
+    await tx.complete;
+    console.log('putDb complete', content);
+  } catch (error) {
+    console.error('putDb not implemented');
+  }
+};
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => console.error('getDb not implemented');

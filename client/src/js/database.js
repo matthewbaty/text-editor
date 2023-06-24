1eeec98888b25a9688d2549f2d547cd512f958b1
Matthew Baty <mattbaty@outlook.com>
2023-06-24 12:39:09 -0400

    // TODO: Add logic for a method that gets all the content from the database finished

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -27,6 +27,17 @@ export const putDb = async (content) => {
 };
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  try {
+    const db = openDB('jate', 1);
+    const tx = db.transaction('jate', 'readonly');
+    const store = tx.objectStore('jate');
+    const content = await store.getAll();
+    console.log('getDb complete', content);
+    return content;
+  } catch (error) {
+    console.error('getDb not implemented'), error;
+  }
+};
 
 initdb();

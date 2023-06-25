8728e5e5296ebf463794f585408561f70c6be38e
Matthew Baty <mattbaty@outlook.com>
2023-06-25 15:40:45 -0400

    more cleanup

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,7 +12,7 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
+
 export const putDb = async (content) => {
   try {
     const db = await openDB('jate', 1);
@@ -26,7 +26,6 @@ export const putDb = async (content) => {
   }
 };
 
-// TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
   try {
     const db = openDB('jate', 1);

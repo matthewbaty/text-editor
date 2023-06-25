cfbc4e1581e00ccdcc7e2fb62cb8f62f61c4e023
Matthew Baty <mattbaty@outlook.com>
2023-06-25 15:40:04 -0400

    code cleanup

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -2,14 +2,13 @@ const butInstall = document.getElementById('buttonInstall');
 let deferredPrompt;
 
 // Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
+
 window.addEventListener('beforeinstallprompt', (event) => {
     event.preventDefault();
     deferredPrompt = event;
     butInstall.style.visibility = 'visible';
 });
 
-// TODO: Implement a click event handler on the `butInstall` element
 butInstall.addEventListener('click', (event) => {
     if (deferredPrompt) {
         deferredPrompt.prompt();
@@ -19,7 +18,6 @@ butInstall.addEventListener('click', (event) => {
     }
 }, true);
 
-// TODO: Add an handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {
     console.log('appinstalled', event)
 });
